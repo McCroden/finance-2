a42ff5775025f2a1c15ab88cb5af3a7988abddfd
mikegee <gee.24@osu.edu>
2012-03-02 17:28:53 -0500

    started model specs

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe Stock do
+
+  it { should belong_to(:user) }
+
+  it { should_not allow_mass_assignment_of(:user_id) }
+
+  [:symbol, :shares, :price].each do |attribute|
+    it { should validate_presence_of(attribute) }
+    it { should allow_mass_assignment_of(attribute) }
+  end
+
+  [10, 10.1234, '10', '10.1234', 0, '0'].each do |val|
+    it { should allow_value(val).for(:price) }
+    it { should allow_value(val).for(:shares) }
+  end
+
+  ['NaN', nil, '', '   '].each do |val|
+    it { should_not allow_value(val).for(:price) }
+    it { should_not allow_value(val).for(:shares) }
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe User do
+
+  let!(:user) { create(:user) }
+
+  it { should validate_presence_of   :email }
+  it { should validate_uniqueness_of :email }
+
+  [:email, :password, :password_confirmation, :remember_me].each do |attribute|
+    it { should allow_mass_assignment_of(attribute) }
+  end
+
+  [:encrypted_password, :reset_password_token, :reset_password_sent_at,
+   :remember_created_at, :sign_in_count, :current_sign_in_at,
+   :sign_in_count, :last_sign_in_at, :current_sign_in_ip,
+   :last_sign_in_ip].each do |attribute|
+    it { should_not allow_mass_assignment_of(attribute) }
+  end
+
+  it { should have_many :stocks }
+
 end
