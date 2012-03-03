d928dc0c6765371aa6f0e0671ec41913e54cbabb
mikegee <gee.24@osu.edu>
2012-03-02 23:07:34 -0500

    added original_values and some style to portfolio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def currency(amount)
+    if amount
+      css_class = 'currency'
+      css_class << ' negative' if amount < 0
+      content_tag(:span, '$%.02f' % amount.abs, :class => css_class)
+    else
+      '<span class="updating">Updating...</span>'.html_safe
+    end
+  end
 end
