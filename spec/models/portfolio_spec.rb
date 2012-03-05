a854caf649f2efc1a69784da9537ed7aa9bdf95d
mikegee <gee.24@osu.edu>
2012-03-05 00:39:59 -0500

    tidying up the Portfolio spec

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,39 +1,38 @@
 require 'spec_helper'
 
+shared_examples_for 'an Array of Stocks' do
+  it 'should delegate .size to the array' do
+    subject.size.should == stocks.size
+  end
+  it 'should delegate .each to the array' do
+    subject.each {|s| s.should be_a(Stock)}
+  end
+end
+
 describe Portfolio do
 
   def a_stock
     build(:stock, price: 10, shares: 1).tap do |s|
-      s.stub(:current_price => 12)
+      s.stub(current_price: 12)
     end
   end
-
-  let(:stocks) { Array.new(5) { a_stock } }
 
   subject { Portfolio.new(stocks) }
 
-  describe 'should behave like an array of Stocks' do
-    its(:size) { should == 5 }
+  context 'with 5 existing stocks' do
+    let(:stocks) { Array.new(5) { a_stock } }
 
-    it 'should delegate .each to the stock array' do
-      subject.each {|s| s.should be_a(Stock)}
-    end
-  end
+    it_behaves_like 'an Array of Stocks'
 
-  describe '.original_value' do
     its(:original_value) { should == 50 }
+    its(:current_value)  { should == 60 }
+    its(:roi)            { should == 10 }
   end
 
-  describe '.current_value' do
-    its(:current_value) { should == 60 }
-  end
+  context 'with some old and a new stock (current_price == nil)' do
+    let(:stocks) { [a_stock, a_stock, build(:stock)] }
 
-  describe '.roi' do
-    its(:roi) { should == 10 }
-  end
-
-  context 'with a new stock (current_price == nil)' do
-    before { subject << build(:stock) }
+    it_behaves_like 'an Array of Stocks'
 
     its(:original_value) { should_not be_nil }
     its(:current_value)  { should     be_nil }
@@ -43,6 +42,8 @@ describe Portfolio do
   context 'with no stocks yet' do
     let(:stocks) { [] }
 
+    it_behaves_like 'an Array of Stocks'
+
     its(:original_value) { should be_zero }
     its(:current_value)  { should be_zero }
     its(:roi)            { should be_zero }
@@ -50,6 +51,8 @@ describe Portfolio do
 
   context 'with just one (new) stock' do
     let(:stocks) { [build(:stock)] }
+
+    it_behaves_like 'an Array of Stocks'
 
     its(:original_value) { should_not be_zero }
     its(:current_value)  { should be_nil }
